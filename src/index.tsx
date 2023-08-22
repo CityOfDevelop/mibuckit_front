4ea0ee584570617ea9de2533007a2db5383f30ef
ImHyunJeong <hyacinth@unopnd.com>
2023-08-22 18:48:38 +0900

    [현정] Add : recoil, react-router-dom, react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const queryClient = new QueryClient(); // 생성
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
